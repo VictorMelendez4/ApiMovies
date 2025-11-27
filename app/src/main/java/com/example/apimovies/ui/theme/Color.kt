8312dc9d10c2f90efeecd63c33accc850b457d5d
Victor Melendez <vicomelbass@gmail.com>
2025-11-27 13:27:09 -0600

    Se modifico Color y Theme para tener colores mas adaptados a la tematica

--- app/src/main/java/com/example/apimovies/ui/theme/Color.kt
+++ app/src/main/java/com/example/apimovies/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.example.apimovies.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryDark = Color(0xFF141A32)
+
+val SecondaryDark = Color(0xFF1E2746)
+
+val AccentColor = Color(0xFFFFC107)
+
+val TextWhite = Color(0xFFFFFFFF)
+
+val TextGray = Color(0xFFB0B3C6)
+
+val HighlightRed = Color(0xFFE50914)
